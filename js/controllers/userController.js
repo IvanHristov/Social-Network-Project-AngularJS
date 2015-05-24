f50ae1ceab80dafcc44ba8b812b0b923694e3d3b
IvanHristov <i.hristov95@gmail.com>
2015-05-24 13:55:00 +0300

    FINAL FINAL

--- js/controllers/userController.js
+++ js/controllers/userController.js
@@ -92,22 +92,6 @@ app.controller("userController", function ($scope, $location, $route,
             })
 
     };
-    //
-    //$scope.showLogin = function() {
-    //    $("#login-form").delay(100).fadeIn(100);
-    //    $("#register-form").fadeOut(100);
-    //    $('#register-form-link').removeClass('active');
-    //    $('#login-form-link').addClass('active');
-    //    //e.preventDefault();
-    //};
-    //
-    //$scope.showRegister = function(){
-    //    $("#register-form").delay(100).fadeIn(100);
-    //    $("#login-form").fadeOut(100);
-    //    $('#login-form-link').removeClass('active');
-    //    $('#register-form-link').addClass('active');
-    //    e.preventDefault();
-    //};
 
     $('#login-form-link').click(function(e) {
         $("#login-form").delay(100).fadeIn(100);
